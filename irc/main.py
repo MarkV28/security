b6244e754e3024a7445da9cc9ed6286d7b849db5
MarkV28 <mark_vels@hotmail.com>
2015-11-21 21:33:14 +0100

    Finished

--- irc/main.py
+++ irc/main.py
@@ -1,23 +1,40 @@
 # -*- coding: UTF-8 -*-
 
 import socket
-import re
+import hashlib
 
-server = "irc.quakenet.org"
-channel = "#testKwawK"
-botnick = "KwawKBot"
+server = "irc.hackthissite.org"
+channel = "#perm8"
+botnick = "KwawK"
 
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ircsock.connect((server, 6667))
 
 ircsock.recv (4096)
-ircsock.send('NICK KwawKPyBot\r\n')
-ircsock.send('USER ' + botnick + ' ' + botnick + ' ' + botnick + ' :Affix IRC\r\n')
-
-while True:
-	data = ircsock.recv (4096)
-	print data
-	if data.find('PING') != -1:
-		ircsock.send('PONG ' + data.split()[1] + '\r\n')
-	if data.find('MODE') != -1:
-		ircsock.send('JOIN ' + channel + '\r\n')
+ircsock.send('NICK ' + botnick + '\r\n')
+ircsock.send('USER ' + botnick + ' ' + botnick + ' ' + botnick + ' :Python IRC Bot\r\n')
+# 
+try:
+	while True:
+		data = ircsock.recv (4096)
+		print data
+		if data.find('PING') != -1:
+			ircsock.send('PONG ' + data.split()[1] + '\r\n')
+		if data.find('MODE ' + botnick + ' :+ix') != -1:
+			ircsock.send('PRIVMSG NickServ identify KwawK Iecanv78\r\n')
+			ircsock.send('NS set autoop on\r\n')
+			ircsock.send('JOIN ' + channel + '\r\n')
+			ircsock.send('NOTICE moo !perm8\r\n')
+		if data.find('!md5') != -1:
+			print str(data[40:56])
+			answer = hashlib.md5(str(data[40:56])).hexdigest()
+			print answer
+			ircsock.send('NOTICE moo !perm8-result ' + answer + '\r\n')
+		if data.find('VERSION') != -1:
+			ircsock.send('NOTICE moo \001VERSION FireDogFuckas:1.0:Hobos\001\r\n')
+		if data.find('!perm8-attack') != -1:
+			ircsock.send('JOIN #takeoverz\r\n')
+			ircsock.send('KICK #takeoverz moo\r\n')
+except KeyboardInterrupt:
+	ircsock.send('DISCONNECT')
+	print "Bye Bye"
